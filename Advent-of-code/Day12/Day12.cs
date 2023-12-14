4f0cdf6f59754b638ac083679b1f64047fddb4ba
Andrew Tootell <andy.tootell@checkatrade.com>
2023-12-14 12:44:43 +0000

    Day12 part one completed

--- Advent-of-code/Day12/Day12.cs
+++ Advent-of-code/Day12/Day12.cs
@@ -15,8 +15,7 @@ public class Day12
 
     [Theory]
     [InlineData(true, 0, 21)]
-    [InlineData(true, 1, 21)]
-    [InlineData(false, 0, 1000)]
+    [InlineData(false, 0, 7344)]
     public void Test_1(bool isTest, int testDataCount, long expectedAnswer)
     {
         var input = ReadInput(12, isTest, testDataCount);
@@ -27,11 +26,9 @@ public class Day12
         {
             var subTotal = RunLogic(springRow.Id, springRow.Row, springRow.Groups);
             total += subTotal;
-            //_testOutputHelper.WriteLine($"ID: {springRow.Id} subTotal: {subTotal} for {springRow.Row} {string.Join(",",springRow.Groups)}");
         });
-        _testOutputHelper.WriteLine("");
         _testOutputHelper.WriteLine($"Total: {total}");
-        //total.Should().Be(expectedAnswer);
+        total.Should().Be(expectedAnswer);
     }
     
     [Theory]
@@ -39,11 +36,25 @@ public class Day12
     [InlineData(false, 0,  0)]
     public void Test_2(bool isTest, int testDataCount, long expectedAnswer)
     {
-        var input = ReadInput(11, isTest, testDataCount);
+        var input = ReadInput(12, isTest, testDataCount);
         var data = ParseData(input);
+        data = UnFold(data);
         long total = 0;
         
-        total.Should().Be(expectedAnswer);
+        data.ForEach(springRow =>
+        {
+            var subTotal = RunLogic(springRow.Id, springRow.Row, springRow.Groups);
+            total += subTotal;
+            WriteLine($"Id: {springRow.Id} subTotal: {subTotal} springRow: {springRow.Row} groups: {string.Join(",",springRow.Groups)}");
+            //WriteLine("");
+        });
+        _testOutputHelper.WriteLine($"Total: {total} < 7427");
+        //total.Should().Be(expectedAnswer);
+    }
+
+    private void WriteLine(string message)
+    {
+        //_testOutputHelper.WriteLine(message);
     }
 
     private int RunLogic(int id, string springRow, List<int> groups)
@@ -56,16 +67,16 @@ public class Day12
         for (var i = (int)Math.Pow(groups.Count + 1, split.Count)-1; i >= 0; i--)
         {
             var toTry = new List<int>();
-            var ttCount = 0;
+            long ttCount = 0;
             for (var j = split.Count-1; j >= 0; j--)
             {
-                var baseForIndex = Convert.ToInt32(Math.Pow(numBase, j));
+                var baseForIndex = Convert.ToInt64(Math.Pow(numBase, j));
                 if (j == 0)
                 {
-                    toTry = toTry.Append(i-ttCount).ToList();
+                    toTry = toTry.Append((int)(i-ttCount)).ToList();
                     break;
                 }
-                toTry = toTry.Append((i-ttCount)/baseForIndex).ToList();
+                toTry = toTry.Append((int)((i-ttCount)/baseForIndex)).ToList();
                 ttCount += toTry.Last() * baseForIndex;
             }
             if (toTry.Sum() == max)
@@ -76,7 +87,9 @@ public class Day12
 
         return count;
     }
-
+    
+    private Dictionary<(string, List<int>),int> cache = new();
+    
     private int TryVariations(int id, List<int> toTry, List<string> split, List<int> groups)
     {
         var subCount = 1;
@@ -84,337 +97,134 @@ public class Day12
         for (var i = 0; i < toTry.Count; i++)
         {
             var groupSize = toTry[i];
-            if (groupSize == 0)
+            int subSubCount;
+            var s = split[i];
+            var g = groups.Skip(previousGroupsSum).Take(groupSize).ToList();
+            if (cache.TryGetValue((s, g), out var value))
             {
-                continue;
+                subSubCount = value;
             }
-
-            var subSubCount = FindPossibilities(id, split[i], groups.Skip(previousGroupsSum).Take(groupSize).ToList());
+            else
+            {
+                _testOutputHelper.WriteLine(".");
+                subSubCount = FindPossibilities(id, s, g);
+                cache.Add((s, g), subSubCount);
+            }
+            
+            if (subSubCount != 0)
+            {
+                WriteLine($"Id: {id} subSubCount: {subSubCount} springRow: {split[i]} groups: {string.Join(",",groups.Skip(previousGroupsSum).Take(groupSize).ToList())}");
+                WriteLine("");
+            }
             subCount *= subSubCount;
-            //_testOutputHelper.WriteLine($"ID: {id} subSubCount:{subSubCount} Count: {subCount} for {split[i]} {string.Join(",",groups.Skip(previousGroupsSum).Take(groupSize).ToList())}");
             previousGroupsSum += groupSize;
         }
-        //_testOutputHelper.WriteLine("");
-        
+        //WriteLine($"Id: {id} subCount: {subCount} springRow: {string.Join(".",split)} groups: {string.Join(",",groups)} toTry: {string.Join(",",toTry)}");
+        //WriteLine("");
         return subCount;
     }
 
     private int FindPossibilities(int id, string springRow, List<int> groups)
     {
-        (springRow, groups) = RemoveWhereMaxHasOnePossibility(springRow, groups);
-        if (!springRow.All(c => c is '#' or '?'))
-        {
-            _testOutputHelper.WriteLine($"ERROR!  ID: {id} for {springRow} {string.Join(',', groups)}");
-        }
-        if (HasNoPossibilities(springRow, groups))
+        if (HasNoPossibilities(id, springRow, groups))
         {
             return 0;
         }
-        
-        if (HasOnePossibility(springRow, groups))
+        if (HasOnePossibility(id, springRow, groups))
         {
             return 1;
         }
-
-        if (!springRow.Contains('#') && groups.Count <= 3)//5)
-        {
-            return AllUnknown(springRow, groups);
-        }
-
-        if (groups.Count == 1)
-        {
-            return SingleGroup(springRow, groups);
-        }
-        
-        (springRow, groups) = RemoveWhereFirstStringMustBeFirstGroupOrLastStringMustBeLastGroup(springRow, groups);
-        if (!springRow.All(c => c is '#' or '?'))
-        {
-            _testOutputHelper.WriteLine($"ERROR!  ID: {id} for {springRow} {string.Join(',', groups)}");
-        }
-        if (HasNoPossibilities(springRow, groups))
-        {
-            return 0;
-        }
-        
-        if (HasOnePossibility(springRow, groups))
-        {
-            return 1;
-        }
-
-        if (!springRow.Contains('#') && groups.Count <= 3)//5)
-        {
-            return AllUnknown(springRow, groups);
-        }
-
-        if (!springRow.Contains('#'))
-        {
-            return 0;
-        }
-
-        if (groups.Count == 1)
-        {
-            return SingleGroup(springRow, groups);
-        }
-
-        if (groups.Count <= 3)
-        {
-            return BruteForce(springRow, groups);
-        }
-        
-        
-        _testOutputHelper.WriteLine($"UNSOLVED! ID: {id} for {springRow} {string.Join(',', groups)}");
-        return 0;
-    }
-
-    private bool HasNoPossibilities(string springRow, List<int> groups)
-    {
-        var springRowToShort = springRow.Length < groups.Sum() + groups.Count - 1;
-        var knownIdMoreThanSum = springRow.Count(c => c == '#') > groups.Sum();
-        var longestHashtag = GroupSpringRowByChar(springRow).Where(s => s.Contains('#')).MaxBy(s => s.Length);
-        var longestKnownIsLongerThanMax = (longestHashtag != null && groups.Count > 0 && longestHashtag.Length > groups.Max());
-        return springRowToShort || knownIdMoreThanSum || longestKnownIsLongerThanMax;
+        return BruteForce(id, springRow, groups);
     }
     
-    private bool HasOnePossibility(string springRow, List<int> groups)
+    private bool HasNoPossibilities(int id, string springRow, List<int> groups)
     {
-        return springRow.Length == 0 || groups.Count == 0 || 
-               !springRow.Contains('?') || 
-               springRow.Length == groups.Sum() + groups.Count - 1;
+        if (springRow.Length < groups.Sum() + groups.Count - 1)
+        {
+            return true;
+        }
+
+        if (springRow.Count(c => c == '#') > groups.Sum())
+        {
+            return true;
+        }
+        return false;
     }
     
-    private (string, List<int>) RemoveWhereMaxHasOnePossibility(string springRow, List<int> groupsParsed)
+    private bool HasOnePossibility(int id,string springRow, List<int> groups)
     {
-        var sections = GroupSpringRowByChar(springRow);
-        while (groupsParsed.Count > 0 && sections.Count(s => s.Contains('#') && s.Length == groupsParsed.Max()) == 1)
+        if (springRow.Length == 0 || groups.Count == 0)
         {
-            var index = sections.FindIndex(s => s.Contains('#') && s.Length == groupsParsed.Max());
-            sections.RemoveAt(index);
-            groupsParsed.Remove(groupsParsed.Max());
-            if (index-1 >= 0)
-            {
-                sections[index - 1] = string.Join("",sections[index - 1].Skip(1));
-                if (string.IsNullOrEmpty(sections[index - 1]))
-                {
-                    sections.RemoveAt(index-1);
-                    index -= 1;
-                }
-            }
-            if (index < sections.Count)
-            {
-                sections[index] = string.Join("",sections[index].Skip(1));
-                if (string.IsNullOrEmpty(sections[index]))
-                {
-                    sections.RemoveAt(index);
-                }
-            }
-            
-            
+            WriteLine("Spring or groups are 0");
+            return true;
         }
 
-        return (string.Join("",sections), groupsParsed);
+        if (!springRow.Contains('?'))
+        {
+            WriteLine("no unknowns");
+            return true;
+        }
+        
+        return false;
+    }
+
+    private int BruteForce(int id, string springRow, List<int> groups)
+    {
+        var lines = RecursiveMakeLines(springRow, groups, new List<int>(Enumerable.Repeat(1, groups.Count-1)), groups.Count-1, 0);
+        var score = lines.Count(line => CanLineMatch(id, line, springRow.ToList()));
+        return score;
+    }
+
+    private List<List<char>> RecursiveMakeLines(string springRow, List<int> groups, List<int> separators, int maxDepth, int depth)
+    {
+        if (depth == maxDepth)
+        {
+            var newLines = new List<List<char>>();
+            var pattern = CreatePattern(groups, separators);
+            var count = 0;
+            while (springRow.Length - pattern.Count-count >= 0)
+            {
+                var line = pattern.Concat(Enumerable.Repeat('.', springRow.Length - pattern.Count-count)).ToList();
+                line = Enumerable.Repeat('.', count).Concat(line).ToList();
+                newLines.Add(line);
+                count++;
+            }
+            return newLines;
+        }
+
+        var lines = new List<List<char>>();
+        while (groups.Sum() + separators.Sum() < springRow.Length)
+        {
+            lines = lines.Concat(RecursiveMakeLines(springRow, groups, separators.ToList(), maxDepth, depth + 1)).ToList();
+            separators[^(maxDepth-depth)] += 1;
+        }
+        lines = lines.Concat(RecursiveMakeLines(springRow, groups, separators.ToList(), maxDepth, depth + 1)).ToList();
+        
+        return lines;
+    }
+
+    private List<char> CreatePattern(List<int> groups, List<int> seperators)
+    {
+        var line = new List<char>();
+        for (var i = 0; i < seperators.Count; i++)
+        {
+            line = line.Concat(Enumerable.Repeat('#', groups[i])).ToList();
+            line = line.Concat(Enumerable.Repeat('.', seperators[i])).ToList();
+        }
+        return line.Concat(Enumerable.Repeat('#', groups.Last())).ToList();
     }
     
-    private List<string> GroupSpringRowByChar(string springRowParsedOnce)
+    private bool CanLineMatch(int id, List<char> line, List<char> pattern)
     {
-        var springRowGroupedByChar = new List<string>();
-        var previousChar = ' ';
-        var currentString = "";
-        foreach (var c in springRowParsedOnce)
+        for (var i = 0; i < line.Count; i++)
         {
-            if (c == previousChar)
+            if (line[i] == '.' && pattern[i] == '#')
             {
-                currentString += c;
-                continue;
+                return false;
             }
-
-            if (previousChar != ' ')
-            {
-                springRowGroupedByChar.Add(currentString);
-            }
-            previousChar = c;
-            currentString = $"{c}";
         }
-        springRowGroupedByChar.Add(currentString);
-
-        return springRowGroupedByChar;
-    }
-
-    private int AllUnknown(string springRow, List<int> groupsParsed)
-    {
-        if (groupsParsed.Count == 1)
-        {
-            return springRow.Length + 1 - groupsParsed.First();
-        }
-        
-        var minPatternSize = (groupsParsed.Sum() + groupsParsed.Count)-1;
-        var triangleHeight = (springRow.Length - minPatternSize) + 1;
-        
-        if (groupsParsed.Count == 2)
-        {
-            return TriangleSize(triangleHeight);
-        }
-
-        var triangleTotal = 0;
-
-        for (var i = 1; i <= triangleHeight; i++)
-        {
-            triangleTotal += TriangleSize(triangleHeight);
-        }
-        
-        return triangleTotal;
-    }
-
-    private int TriangleSize(int triangleHeight)
-    {
-        return triangleHeight * (triangleHeight+1)/2;
-    }
-    
-    private int SingleGroup(string springRow, List<int> groups)
-    {
-        var sections = GroupSpringRowByChar(springRow);
-        var min = groups.Min();
-        var firstSet = sections.FindIndex(s => s.Contains('#'));
-        var lastSet = sections.FindLastIndex(s => s.Contains('#'));
-
-        if (firstSet != lastSet)
-        {
-            // ??????#?#??????
-            min -= sections.Skip(firstSet).Take(lastSet - firstSet).ToList().Sum(s=>s.Length);
-        }
-        
-        if (firstSet > 0)
-        {
-            // ???# => 4
-            min = sections[firstSet - 1].Length+1 < min
-                ? sections[firstSet - 1].Length+1
-                : min;
-        }
-        if (lastSet < sections.Count-1)
-        {
-            // #?? => 3
-            min = sections[lastSet + 1].Length+1 < min
-                ? sections[lastSet + 1].Length+1
-                : min;
-        }
-
-        return min;
-    }
-
-    private int BruteForce(string springRow, List<int> groups)
-    {
-        var max = AllUnknown(springRow, groups);
-        //????#?? 1,4
-        return max;
-    }
-    
-    private (string, List<int>) RemoveWhereFirstStringMustBeFirstGroupOrLastStringMustBeLastGroup(
-        string springRow, List<int> groups)
-    {
-        var sections = GroupSpringRowByChar(springRow);
-        while (sections.Count > 0 &&
-               groups.Count > 0)
-        {
-            sections = GroupSpringRowByChar(string.Join("",sections));
-            
-            if (string.IsNullOrEmpty(sections.First()))
-            {
-                sections = sections.Skip(1).ToList();
-                continue;
-            }
-            
-            if (string.IsNullOrEmpty(sections.Last()))
-            {
-                sections = sections.Take(sections.Count-1).ToList();
-                continue;
-            }
-
-            if (sections.First().Contains('#'))
-            {
-                (sections, groups) = RemoveStartingHashtags(sections, groups);
-                continue;
-            }
-
-            if (sections.Last().Contains('#'))
-            {
-                (sections, groups) = RemoveEndingHashtags(sections, groups);
-                continue;
-            }
-            break;
-        }
-
-        return (string.Join("",sections), groups);
-    }
-
-    private (List<string>, List<int>) RemoveStartingHashtags(
-        List<string> sections, List<int> groups)
-    {
-        // remove group
-        var firstStringGroup = sections.First();
-        var firstGroup = groups.First();
-        sections = sections.Skip(1).ToList();
-        groups = groups.Skip(1).ToList();
-        if (firstStringGroup.Length == firstGroup)
-        {
-            // reduce next group by 1
-            sections[0] = string.Join("",sections.First().Skip(1));
-            return (sections,groups);
-        }
-
-        // first group must be more than first line and next line must be ?'s
-        // +1 to the group for the .
-        var howManyMoreToRemove = firstGroup + 1 - firstStringGroup.Length;
-        while (howManyMoreToRemove > 0 && sections.Count > 0)
-        {
-            // reduce line by 1
-            var newFirst = string.Join("",sections.First().Skip(1));
-            if (!string.IsNullOrEmpty(newFirst))
-            {
-                sections[0] = newFirst;
-            }
-            else
-            {
-                sections = sections.Skip(1).ToList();
-            }
-
-            howManyMoreToRemove--;
-        }
-
-        return (sections, groups);
-    }
-
-    private (List<string>, List<int>) RemoveEndingHashtags(
-        List<string> sections, List<int> groups)
-    {
-        // remove group
-        var lastStringGroup = sections.Last();
-        var lastGroup = groups.Last();
-        sections = sections.Take(sections.Count-1).ToList();
-        groups = groups.Take(groups.Count-1).ToList();
-        if (lastStringGroup.Length == lastGroup)
-        {
-            // reduce previous group by 1
-            sections[^1] = string.Join("",sections.Last().Skip(1));
-            return (sections, groups);
-        }
-                
-        // last group must be more than last line and previous line must be ?'s
-        // +1 to the group for the .
-        var howManyMoreToRemove = lastGroup + 1 - lastStringGroup.Length;
-        while (howManyMoreToRemove > 0  && sections.Count > 0)
-        {
-            // reduce line by 1
-            var newLast = string.Join("",sections.Last().Skip(1));
-            if (!string.IsNullOrEmpty(newLast))
-            {
-                sections[^1] = newLast;
-            }
-            else
-            {
-                sections = sections.Take(sections.Count - 1).ToList();
-            }
-            howManyMoreToRemove--;
-        }
-        return (sections, groups);
+        WriteLine($"id: {id} line: {string.Join("",line)} pattern: {string.Join("",pattern)}");
+        return true;
     }
     
     private class SpringRow
@@ -423,7 +233,24 @@ public class Day12
         public string Row;
         public List<int> Groups;
     }
-    
+
+    /*
+     * To unfold the records,
+     * on each row,
+     * replace the list of spring conditions with five copies of itself (separated by ?) and
+     * replace the list of contiguous groups of damaged springs with five copies of itself (separated by ,).
+     */
+    private List<SpringRow> UnFold(List<SpringRow> data)
+    {
+        foreach (var springRow in data)
+        {
+            springRow.Row = springRow.Row +"?"+ springRow.Row +"?"+ springRow.Row +"?"+ springRow.Row +"?"+ springRow.Row;
+            springRow.Groups = springRow.Groups.Concat(springRow.Groups).Concat(springRow.Groups).Concat(springRow.Groups).Concat(springRow.Groups).ToList();
+        }
+
+        return data;
+    }
+
     private List<SpringRow> ParseData(List<string> input)
     {
         var data = new List<SpringRow>();
